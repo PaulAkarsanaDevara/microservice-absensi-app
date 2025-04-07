b634323cc041373a7101a4bbb67d8cb666bbf34a
Farhan <101630148+KucingOyenBarbar@users.noreply.github.com>
2025-04-08 06:24:02 +0700

    add database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
-
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+      playground: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '******',
+      port: 5432,
+      username: '*****',
+      password: '*****',
+      database: '*****',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
